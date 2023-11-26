9212212dd77b50ce3a5406cbb72178aafbe6654a
InvisEA <geniusser@mail.ru>
2023-11-26 10:41:55 -0500

    Added implementation for 'List all prodcuts' and 'Delete a product' endpoints that pass corresponding tests

--- service/routes.py
+++ service/routes.py
@@ -94,9 +94,19 @@ def create_products():
 # L I S T   A L L   P R O D U C T S
 ######################################################################
 
-#
-# PLACE YOUR CODE TO LIST ALL PRODUCTS HERE
-#
+@app.route("/products", methods=["GET"])
+def list_products():
+    """
+    List all Products
+    This endpoint will list all Products that are in database
+    """
+    app.logger.info("Request to List all Products...")
+
+    found = Product.all()
+    results = [result.serialize() for result in found]
+    app.logger.info("[%s] Products returned", len(results))
+
+    return results, status.HTTP_200_OK
 
 ######################################################################
 # R E A D   A   P R O D U C T
@@ -150,7 +160,20 @@ def update_products(product_id):
 # D E L E T E   A   P R O D U C T
 ######################################################################
 
+@app.route("/products/<int:product_id>", methods=["DELETE"])
+def delete_products(product_id):
+    """
+    Delete a Product
+    This endpoint will delete a Product based on the body that is posted
+    """
+    app.logger.info("Request to Delete a product with id [%s]", product_id)
 
-#
-# PLACE YOUR CODE TO DELETE A PRODUCT HERE
-#
+    found = Product.find(product_id)
+    if not found:
+        abort(
+            status.HTTP_404_NOT_FOUND,
+            f"Product with id '{product_id}' does not exist"
+        )
+    found.delete()
+
+    return "", status.HTTP_204_NO_CONTENT
