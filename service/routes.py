7a914e876981e0cc8342992c9d49cb3859dcc7fe
InvisEA <geniusser@mail.ru>
2023-11-26 12:35:32 -0500

    Added implementation of retrieving products by name, category and availability by extending 'GET /products' endpoint

--- service/routes.py
+++ service/routes.py
@@ -20,7 +20,7 @@ Product Store Service with UI
 """
 from flask import jsonify, request, abort
 from flask import url_for  # noqa: F401 pylint: disable=unused-import
-from service.models import Product
+from service.models import Product, Category
 from service.common import status  # HTTP Status Codes
 from . import app
 
@@ -96,16 +96,29 @@ def create_products():
 
 @app.route("/products", methods=["GET"])
 def list_products():
-    """
-    List all Products
-    This endpoint will list all Products that are in database
-    """
-    app.logger.info("Request to List all Products...")
+    """Returns a list of Products"""
+    app.logger.info("Request to list Products...")
+    products = []
+    name = request.args.get("name")
+    category = request.args.get("category")
+    available = request.args.get("available")
 
-    found = Product.all()
-    results = [result.serialize() for result in found]
+    if name:
+        app.logger.info("Find by name: %s", name)
+        products = Product.find_by_name(name)
+    elif category:
+        app.logger.info("Find by category: %s", category)
+        category_value = getattr(Category, category.upper())
+        products = Product.find_by_category(category_value)
+    elif available:
+        app.logger.info("Find by availability: %s", available)
+        available_value = available.lower() in ["true", "yes", "1"]
+        products = Product.find_by_availability(available_value)
+    else:
+        app.logger.info("Find all")
+        products = Product.all()
+    results = [product.serialize() for product in products]
     app.logger.info("[%s] Products returned", len(results))
-
     return results, status.HTTP_200_OK
 
 ######################################################################
