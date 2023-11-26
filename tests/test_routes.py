4b9e8c2c21514b0569f1ecce5e9d33bc04dd8a46
InvisEA <geniusser@mail.ru>
2023-11-26 06:36:32 -0500

    Added tests for assumed behaviour of 'GET /products/{id}' endpoint

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -163,9 +163,21 @@ class TestProductRoutes(TestCase):
         response = self.client.post(BASE_URL, data={}, content_type="plain/text")
         self.assertEqual(response.status_code, status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
 
-    #
-    # ADD YOUR TEST CASES HERE
-    #
+    def test_get_product(self):
+        """It should get a product with a required ID"""
+        products = self._create_products(1)
+        test_product = products[0]
+        response = self.client.get(f"{BASE_URL}/{test_product.id}")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        data = response.get_json()
+        self.assertEqual(data["name"], test_product.name)
+
+    def test_get_product_not_found(self):
+        """It should not GET a product that was not found"""
+        response = self.client.get(f"{BASE_URL}/0")
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+        data = response.get_json()
+        self.assertIn("does not exist", data["message"])
 
     ######################################################################
     # Utility functions
